6a5fb2669065408e873e09cf0dddf257b148ac6b
mazharuddin <mazharuddin1305@gmail.com>
2026-06-27 17:17:36 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -1,35 +1,15 @@
-// import express, { urlencoded } from "express";
-// import errorMiddleware from "./src/middleware/error.middleware.js";
-// import authRoutes from "./src/modules/auth/auth.routes.js"
-// import connectDB from "./src/config/db.js";
-// import dotenv from "dotenv"
-// dotenv.config();
-// const app = express()
-
-// connectDB()
-
-
-// app.use(urlencoded({ extended: false }))
-// app.use(express.json())
-
-
-// app.use("/auth", authRoutes)
-// app.use(errorMiddleware)
-
-// app.listen(3000, () => {
-//   console.log("running");
-
-// })
-
-
-
 import express, { urlencoded } from "express";
 import errorMiddleware from "./middleware/error.middleware.js";
 import authRoutes from "./modules/auth/auth.routes.js";
+import cors from "cors"
 
 const app = express();
 
 // Middlewares
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 app.use(urlencoded({ extended: false }));
 app.use(express.json());
 
