fb9b076342a62066a79a26ccba4863f18f184d50
mazharuddin <mazharuddin1305@gmail.com>
2026-06-29 22:12:02 +0530

    added auth to server crud

--- src/modules/admin/server/server.routes.js
+++ src/modules/admin/server/server.routes.js
@@ -1,16 +1,17 @@
 import { createServer, getAllServers, getServerById, updateServer, deleteServer } from "./server.controller.js";
 import { validate } from "../../../middleware/validate.middleware.js";
 import { createServerValidation } from "./server.validation.js";
+import { authenticate } from "../../../middleware/auth.middleware.js";
 import express from "express";
 const router = express.Router()
 
 // Base endpoint: /api/v1/admin/server
-router.post("/", validate(createServerValidation), createServer)
-router.get("/", getAllServers);
+router.post("/", authenticate, validate(createServerValidation), createServer)
+router.get("/", authenticate, getAllServers);
 
 // Parameterized endpoint: /api/v1/admin/server/:id
-router.get("/:id", getServerById);
-router.put("/:id", updateServer);
-router.delete("/:id", deleteServer);
+router.get("/:id", authenticate, getServerById);
+router.put("/:id", authenticate, updateServer);
+router.delete("/:id", authenticate, deleteServer);
 
 export default router
